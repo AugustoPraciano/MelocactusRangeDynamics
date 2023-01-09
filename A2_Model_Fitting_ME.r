de3e10b33b9893e47c6272cbf9b48ec52adfa0a8
AugustoPraciano <cesar.praciano@gmail.com>
2023-01-09 15:52:28 -0300

    Melocactus File

--- A2_Model_Fitting_ME.r
+++ A2_Model_Fitting_ME.r
@@ -5,10 +5,9 @@
 #######################################################################################
 ##############################   MODEL FITTING ########################################
 #######################################################################################
-### Esse é o script principal, usado para ajustar um modelo de distribuição Maxent e 
-### tranferi-lo para cenários futuros de mudanças climáticas. A AUC é usada para
-### avaliar os modelos; para isso os dados de espécies sao particionados em dados 
-### de treino e teste.
+### This is the main script used to fit a Maxent distribution model and transfer it to 
+### future climate change scenarios. AUC is used to evaluate the models. For this
+### the species data are partitioned into training and test data
 #######################################################################################
 ###################################  Index  ###########################################
 ### 1 - Load packages
@@ -44,8 +43,8 @@ data(wrld_simpl) # countries boundaries
 #######################################################################################
 ############## 2 - Load data(predictors layers, presences & background) ###############
 #######################################################################################
-##### os dados preparados no apêndice 1 devem estar contidos nos seus respectivos ##### 
-################# diretórios, portanto, primeiro defina os diretórios #################
+##### Data that was prepared in "A1 Data Preparation" must be contained in their ###### 
+##################### directories, so first define the directories ####################
 
 # Define the extent of a rectangular study area: lon 45W 34W; lat 2S 17S
 ext = extent(-45, -34, -17, -2)
